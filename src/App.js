6314d4e0d9d423d1799178dbe0199c8c4de95db9
srishti-maurya <srishti22maurya@gmail.com>
2021-12-06 13:17:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactRouterSetup from "./tutorial/11-react-router/index";
+function App() {
+  return (
+    <div className="container">
+      <h2>Advanced Tutorial</h2>
+      <ReactRouterSetup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tutorial/3-conditional-rendering/2-short-circuit.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const ShortCircuit = () => {
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  return (
+    <>
+      <h1>{text || "john doe"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ShortCircuit;
